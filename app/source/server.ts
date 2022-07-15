df5e89832ad9f83ba7dd30810ebb88ab0372f7b8
Gabriel Morcatti <gabriel.morcatti@pelando.com.br>
2022-07-15 18:46:40 -0300

    feat: Import routes and start dotenv

--- app/source/server.ts
+++ app/source/server.ts
@@ -1,8 +1,13 @@
 import express from 'express'
 
+import 'dotenv/config'
+
+import router from '~src/routes'
+
 const app = express()
 
 app.use(express.json())
+app.use(router)
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
