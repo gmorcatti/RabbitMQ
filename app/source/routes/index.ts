969c8f41c408502d87db054ec368453c82b0e7eb
Gabriel Morcatti <gabriel.morcatti@pelando.com.br>
2022-07-18 18:05:27 -0300

    feat: Route to consume messages

--- app/source/routes/index.ts
+++ app/source/routes/index.ts
@@ -8,5 +8,6 @@ const router = Router()
 const messageController = new MessageController()
 
 router.post('/send-message', messageController.sendMessage)
+router.get('/read-message', messageController.consumeMessage)
 
 export default router
