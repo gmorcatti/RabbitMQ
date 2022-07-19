36dbed860a58def0c5ccf00f843330952885a6c4
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2022-07-19 17:57:47 -0300

    feat: Create Exchange add in Controller

--- app/source/controllers/message.controller.ts
+++ app/source/controllers/message.controller.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from 'express'
 
 import { ConsumeMessageService } from '~src/services/consumeMessage.service'
+import { CreateExchangeService } from '~src/services/createExchange.service'
 import { CreateQueueService } from '~src/services/createQueue.service'
 import { SendMessageService } from '~src/services/sendMessage.service'
 
 const sendMessageService = new SendMessageService()
 const consumeMessageService = new ConsumeMessageService()
 const createQueueService = new CreateQueueService()
+const createExchangeService = new CreateExchangeService()
 
 export class MessageController {
   async send (req: Request, res: Response) {
@@ -21,6 +23,11 @@ export class MessageController {
 
   async createQueue (req: Request, res: Response) {
     await createQueueService.handle(req.body.name, req.body.options)
+    return res.send('OK')
+  }
+
+  async createExchange (req: Request, res: Response) {
+    await createExchangeService.handle(req.body.name, req.body.type, req.body.options)
     return res.send('OK')
   }
 }
