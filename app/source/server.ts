4c4f953ffd1b0f39d8d46f077c354f689c3853d3
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2022-07-19 17:35:16 -0300

    feat: use errorHandler on express

--- app/source/server.ts
+++ app/source/server.ts
@@ -3,12 +3,14 @@ import 'express-async-errors'
 
 import express from 'express'
 
+import { ErrorHandler } from '~config/errors/ErrorHandler'
 import router from '~src/routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
+app.use(ErrorHandler)
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
