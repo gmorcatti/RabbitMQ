6adf8b9ea3daba4e13650227b472af8725b19e4a
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2022-07-19 17:35:56 -0300

    fix: Throw AppError instead of Error on service

--- app/source/services/createQueue.service.ts
+++ app/source/services/createQueue.service.ts
@@ -1,10 +1,11 @@
 import env from '~config/env'
+import { AppError } from '~config/errors/AppError'
 import RabbitMQServer from '~infra/rabbitmq'
 
 export class CreateQueueService {
   async handle (queue: string) {
     if (!queue) {
-      throw new Error("Queue name wasn't informed")
+      throw new AppError("Queue name wasn't informed")
     }
 
     const server = new RabbitMQServer(env.rabbitMQ.uri)
