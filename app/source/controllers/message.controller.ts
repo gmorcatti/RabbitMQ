965aa9cc3ababf3a31449db469677e517f37623c
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2022-07-19 17:21:42 -0300

    feat: Add createQueue method on Controller

--- app/source/controllers/message.controller.ts
+++ app/source/controllers/message.controller.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express'
 
 import { ConsumeMessageService } from '~src/services/consumeMessage.service'
+import { CreateQueueService } from '~src/services/createQueue.service'
 import { SendMessageService } from '~src/services/sendMessage.service'
 
 const sendMessageService = new SendMessageService()
 const consumeMessageService = new ConsumeMessageService()
+const createQueueService = new CreateQueueService()
+
 export class MessageController {
   async send (req: Request, res: Response) {
     sendMessageService.handle()
@@ -13,6 +16,11 @@ export class MessageController {
 
   async consume (_: Request, res: Response) {
     consumeMessageService.handle()
+    return res.send('OK')
+  }
+
+  async createQueue (req: Request, res: Response) {
+    createQueueService.handle(req.body.name)
     return res.send('OK')
   }
 }
